3a0fc7d1caf30cbe7501bdb08dfbe7cbc4c47ec8
Oscar A. Esquivel Oviedo <oscar.esquiveloviedo@fiserv.com>
2019-10-04 15:38:39 -0600

    RecyclerView 9: Added a new recycler view to the user table layout and a method for setting up said recycler view with the previously created classes simply providing a list of users (obtained from the "user data provider"). Also set the height of both view inside the recycler view as "wrap_content" so each item does not occupy the whole screen (very important).

--- app/src/main/java/com/example/trainingapp/activities/UserTableActivity.java
+++ app/src/main/java/com/example/trainingapp/activities/UserTableActivity.java
@@ -3,15 +3,24 @@ package com.example.trainingapp.activities;
 import android.os.Bundle;
 
 import com.example.trainingapp.R;
+import com.example.trainingapp.UserTableAdapter;
+import com.example.trainingapp.data.User;
+import com.example.trainingapp.data.UserDataProvider;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
+import java.util.List;
+
 public class UserTableActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private UserTableAdapter userTableAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +28,7 @@ public class UserTableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_table);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        setupRecyclerView();
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,4 +39,11 @@ public class UserTableActivity extends AppCompatActivity {
         });
     }
 
+    private void setupRecyclerView() {
+        List<User> users = UserDataProvider.getSingletonInstance().getUsers();
+        recyclerView = findViewById(R.id.user_table_recycler_view);
+        userTableAdapter = new UserTableAdapter(this, users);
+        recyclerView.setAdapter(userTableAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 }
