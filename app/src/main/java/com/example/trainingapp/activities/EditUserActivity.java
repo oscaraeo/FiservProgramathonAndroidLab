df064c08969380300206cd5c327cffed39a07437
Oscar A. Esquivel Oviedo <oscar.esquiveloviedo@fiserv.com>
2019-10-04 12:14:45 -0600

    Copied the layout from the view user activity to the edit user activity (except for the avatar image), as well as the code for loading the data, to display the user details. Also used new identifiers for these copied views and created new strings for them.

--- app/src/main/java/com/example/trainingapp/activities/EditUserActivity.java
+++ app/src/main/java/com/example/trainingapp/activities/EditUserActivity.java
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 
 public class EditUserActivity extends AppCompatActivity {
     private User user;
@@ -26,6 +27,9 @@ public class EditUserActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String userId = intent.getStringExtra("INTENT_EXTRA_USER_ID");
         user = UserDataProvider.getSingletonInstance().getUserWithID(userId);
+        if (user != null) {
+            loadUserData();
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -36,5 +40,14 @@ public class EditUserActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void loadUserData() {
+        TextView firstName = findViewById(R.id.edit_user_first_name);
+        TextView lastName = findViewById(R.id.edit_user_last_name);
+        TextView email = findViewById(R.id.edit_user_email);
+        firstName.setText(user.getFirstName());
+        lastName.setText(user.getLastName());
+        email.setText(user.getEmail());
     }
 }
