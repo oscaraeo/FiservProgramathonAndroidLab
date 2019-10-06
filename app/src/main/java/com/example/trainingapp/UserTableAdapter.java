d85fe0960a1ec21876ecbf4183389092912de06f
Oscar A. Esquivel Oviedo <oscar.esquiveloviedo@fiserv.com>
2019-10-05 19:03:26 -0600

    Retrofit 5: Changed the code in the adapter and the view holder so the recycler view reacts to taps on the full name text view. Now when the user taps on the text view, this is picked up by the view holder class, which alerts the adapter by sending its position, and the adapter uses it to obtain the user that was selected by the user and create a new intent to the view user activity, putting the selected user's identifier as an extra in said intent.

--- app/src/main/java/com/example/trainingapp/UserTableAdapter.java
+++ app/src/main/java/com/example/trainingapp/UserTableAdapter.java
@@ -2,11 +2,13 @@ package com.example.trainingapp;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.trainingapp.activities.ViewUserActivity;
 import com.example.trainingapp.data.User;
 
 import java.util.List;
@@ -17,8 +19,10 @@ import androidx.recyclerview.widget.RecyclerView;
 public class UserTableAdapter extends RecyclerView.Adapter {
     private LayoutInflater inflater;
     private List<User> users;
+    private Context context;
 
     public UserTableAdapter(Context context, List<User> users) {
+        this.context = context;
         inflater = LayoutInflater.from(context);
         this.users = users;
     }
@@ -34,7 +38,9 @@ public class UserTableAdapter extends RecyclerView.Adapter {
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         User user = users.get(position);
         String userFullName = user.getFirstName() + " " + user.getLastName();
-        ((UserViewHolder)holder).userName.setText(userFullName);
+        UserViewHolder viewHolder = (UserViewHolder)holder;
+        viewHolder.userName.setText(userFullName);
+        viewHolder.position = position;
     }
 
     @Override
@@ -42,18 +48,28 @@ public class UserTableAdapter extends RecyclerView.Adapter {
         return users.size();
     }
 
+    private void goToViewUserForViewHolderAtPosition(Integer position) {
+        User user = users.get(position);
+        Intent goToViewUser = new Intent(context, ViewUserActivity.class);
+        goToViewUser.putExtra("USER_ID", user.getId());
+        context.startActivity(goToViewUser);
+    }
+
     public class UserViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private Integer position;
         private TextView userName;
+        private UserTableAdapter adapter;
 
         private UserViewHolder(View itemView, UserTableAdapter adapter) {
             super(itemView);
             userName = itemView.findViewById(R.id.recycler_view_item_user_name);
             userName.setOnClickListener(this);
+            this.adapter = adapter;
         }
 
         @Override
         public void onClick(View v) {
-
+            adapter.goToViewUserForViewHolderAtPosition(position);
         }
     }
 }
